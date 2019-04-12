ea935f1686f91aa0c25d4e9d773adf48306f3538
Abhishek Chavan <37843779+abhkchavan@users.noreply.github.com>
2019-04-12 16:00:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const db = require('./queries')
+var cors = require('cors')
+
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
+
+
+app.use((request,respose,next)=>{
+respose.header("Acess-Control-Allow-Origin","*");
+respose.header("Acess-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+next();
+})
+
+
 
 app.listen(port,()=>{
     console.log("port running on 3000");
