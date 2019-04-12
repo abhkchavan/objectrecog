440abf6ec4258224891f8fc76fce317220c60069
Abhishek Chavan <37843779+abhkchavan@users.noreply.github.com>
2019-04-12 13:03:09 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const db = require('./queries')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.listen(port,()=>{
+    console.log("port running on 3000");
+})
+
+app.get('/',(request,response)=>{
+    response.json({info: 'Node.js, Express, and Postgres API'})
+})
+
+app.get('/product', db.getProduct)
